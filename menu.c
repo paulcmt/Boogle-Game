01fcdcdb8ac4ea6d4a71f1a65b6bcde10013939b
paulcmt <paulcleent@gmail.com>
2022-04-14 16:54:27 +0200

    premier commit & push (menu)

---
+++ menu.c
@@ -0,0 +1,10 @@
+#include "menu.h"
+
+void menu()
+{
+    printf("--------------------------------\n");
+    printf("1. Démarrer une partie\n");
+    printf("2. Afficher les meilleurs scores\n");
+    printf("3. Quitter\n");
+    printf("--------------------------------");
+}
---
+++ menu.h
@@ -0,0 +1,13 @@
+//
+// Created by Paul CLEMENT on 14/04/2022.
+//
+
+#ifndef PROJET_BOGGLE_MENU_H
+#define PROJET_BOGGLE_MENU_H
+
+#endif //PROJET_BOGGLE_MENU_H
+
+#include "stdio.h"
+#include "stdlib.h"
+
+void menu();
