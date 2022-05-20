451578d6ad48169c62476ee2eeeca1f9c2b3fcef
paulcmt <paulcleent@gmail.com>
2022-05-20 09:58:34 +0200

    Problème résolu
    To do : faire les autres options du menu des scores

--- main.c
+++ main.c
@@ -62,7 +62,7 @@ int main()
     do
     {
         // Utilisation du fichier "menu.c"
-        choix = Menu(); // Demande du choix de l'utilisateur
+        choix = 1;//Menu(); // Demande du choix de l'utilisateur
         system("clear");
 
         // Conséquence du choix
@@ -71,14 +71,71 @@ int main()
             case 1: // Lancement de la partie
 
                 // Utilisation du fichier "generation grille.c"
-                longueur = Dimension_grille(); // Demande dimension de la grille
+                longueur = 7; //Dimension_grille(); // Demande dimension de la grille
 
                 /** Debut du bloc "Definition de la grille" **/
                 char grille[8][8];
+                /*
+                grille[0][0] = 'I';
+                grille[0][1] = 'O';
+                grille[0][2] = 'G';
+                grille[0][3] = 'E';
+                grille[0][4] = 'O';
+                grille[0][5] = 'R';
+                grille[0][6] = 'N';
+
+                grille[1][0] = 'N';
+                grille[1][1] = 'P';
+                grille[1][2] = 'A';
+                grille[1][3] = 'D';
+                grille[1][4] = 'I';
+                grille[1][5] = 'T';
+                grille[1][6] = 'S';
+
+                grille[2][0] = 'S';
+                grille[2][1] = 'U';
+                grille[2][2] = 'R';
+                grille[2][3] = 'S';
+                grille[2][4] = 'P';
+                grille[2][5] = 'L';
+                grille[2][6] = 'E';
+
+                grille[3][0] = 'I';
+                grille[3][1] = 'D';
+                grille[3][2] = 'E';
+                grille[3][3] = 'X';
+                grille[3][4] = 'T';
+                grille[3][5] = 'A';
+                grille[3][6] = 'M';
+
+                grille[4][0] = 'O';
+                grille[4][1] = 'G';
+                grille[4][2] = 'P';
+                grille[4][3] = 'T';
+                grille[4][4] = 'A';
+                grille[4][5] = 'Q';
+                grille[4][6] = 'I';
+
+                grille[5][0] = 'R';
+                grille[5][1] = 'A';
+                grille[5][2] = 'S';
+                grille[5][3] = 'V';
+                grille[5][4] = 'C';
+                grille[5][5] = 'U';
+                grille[5][6] = 'L';
+
+                grille[6][0] = 'I';
+                grille[6][1] = 'V';
+                grille[6][2] = 'E';
+                grille[6][3] = 'M';
+                grille[6][4] = 'D';
+                grille[6][5] = 'E';
+                grille[6][6] = 'F';*/
+
                 Generation_grille(grille, longueur);
                 /** Fin du bloc "Definition de la grille" **/
 
-                int temps_limite = Temps_de_la_partie(); // Demande du temps pour le jeu à l'utilisateur
+                int temps_limite = 60;//Temps_de_la_partie(); // Demande du temps pour le jeu à l'utilisateur
 
                 /** Début "Création tableau dynamique pour la saisie des mots" **/
 
