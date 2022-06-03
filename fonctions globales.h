90a6921e8cd7abf9d9e5f9d0fed58266038b4c1d
paulcmt <paulcleent@gmail.com>
2022-06-03 19:25:42 +0200

    FIN DU PROJET !

--- fonctions globales.h
+++ fonctions globales.h
@@ -11,6 +11,23 @@
 #include <unistd.h>
 #include <math.h>
 
+/**
+ * Fonction comptant le nombre de lettre dans un tableau de lettres
+ * @param Tabl
+ * @param lettre
+ * @return compteur
+ */
 int Comptage_lettre_tableau(char Tabl[], char lettre);
+
+/**
+ * Fonction intialisant un tableau de lettre
+ * @param tabl
+ */
 void Initialisation_tableau(char tabl[]);
+
+/**
+ * Fonction affichant la grille selon une dimension
+ * @param grille
+ * @param longueur
+ */
 void Affichage_grille(char grille[8][8], int longueur); // Permet d'afficher la grille depuis n'importe quel fichier
